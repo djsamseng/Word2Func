3f4346fe9e8903beddd0b46589ef3859617b8748
Samuel Seng <djsamseng@gmail.com>
2015-10-21 23:06:23 -0400

    Added Google Custom Search

--- Webserver/project/backend/search.py
+++ Webserver/project/backend/search.py
@@ -1,2 +1,14 @@
+import requests
+
+API_KEY = 'AIzaSyB0jaur8fE5AMTxWHrTYGp--N8BWgd4CFU'
+CX_KEY = '016888903529204161084:udpb71g1u8a'
+API_URL = 'https://www.googleapis.com/customsearch/v1'
+
 def get_urls(query):
-    return 'Test url'
+    params = { 'key' : API_KEY, 'cx' : CX_KEY, 'q' : query }
+    r = requests.get(API_URL, params = params)
+    json = r.json()
+    results = []
+    for item in json['items']:
+        results.append({ 'title':item['title'], 'snippet':item['snippet'], 'link':item['link']})
+    return results
--- Webserver/project/views.py
+++ Webserver/project/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, request
 from project import app
 from backend import search
+import json
 
 @app.route('/')
 def index():
-    return render_template('index.html', urls=search.get_urls('test'))
+    urls = "empty"
+    return render_template('index.html', urls=urls)
+
+@app.route('/get_links', methods=['POST'])
+def get_links():
+    return json.dumps(search.get_urls(request.json['query']))
+    
