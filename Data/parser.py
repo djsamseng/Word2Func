1ddaa1d9d8adc6bed3bafba17b0a92f2e37c5948
Samuel Seng <djsamseng@gmail.com>
2015-10-26 15:45:52 -0400

    Fixed multiple spaces. Data file indexes by url

--- Data/parser.py
+++ Data/parser.py
@@ -1,5 +1,3 @@
-import requests
-
 def is_visible(elem):
     import re
     if elem.parent.name in ['style', 'script', '[document]', 'head', 'title']:
@@ -12,19 +10,26 @@ def url_to_file(url, filename):
     import os.path
     import json
     import bs4
+    import requests
 
-    data = []
+    data = dict()
     if os.path.isfile(filename):
         with open(filename) as f:
             data = json.loads(f.read())
     r = requests.get(url)
 
     req_text = ''.join([w if ord(w) < 128 and ord(w) > 31 else ' ' for w in r.text.encode('utf-8')])
-    soup = bs4.BeautifulSoup(req_text)
+    soup = bs4.BeautifulSoup(req_text, 'html.parser')
     text_list = soup.findAll(text=True)
-    text = ''.join(filter(is_visible, text_list)).encode('ascii', 'ignore').replace('\n', ' ').replace('\t', ' ')
-
+    text = ' '.join(filter(is_visible, text_list))
+    text = text.encode('ascii', 'ignore')
+    text = text.replace('\n', ' ').replace('\t', ' ')
+    text = ' '.join(text.split())
     
-    data.append({'url':url, 'text':text})
+    data[url] = text
     with open(filename, 'w') as f:
         f.write(json.dumps(data))
+
+if __name__ == '__main__':
+    url_to_file('https://en.wikipedia.org/wiki/Tom_Brady', 'data.json')
+    
